cb64746a0fbb6a850fa5be2f6723356dbdeeec59
ashis <ashis@ASHISH>
2024-01-25 00:03:44 -0500

    Add project files.

---
+++ WebApplication1/Controllers/ProjectsController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class ProjectsController : Controller
+    {
+        public IActionResult Index()
+        {
+            var projects = new List<Project>()
+            {
+                new Project { ProjectId = 1, Name = "First 1", Description = "First Project" },
+                new Project { ProjectId = 2, Name = "First 2", Description = "Second Project" }
+            };
+
+            return View(projects);
+        }
+
+        public IActionResult Create()
+        {
+           
+            return View();
+        }
+
+        public IActionResult Details()
+        {
+            return View();
+        }
+
+    }
+}
